d1c6895358f1f25a8c98df2040944a42a5241649
Abdurrahman Abolaji <abolajiabdurrahman@gmail.com>
2022-01-11 11:59:12 +0100

    understanding the DOM

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 'use strict';
 
+// The DOM is a complete structured representation of the HTML document. DOM methods and properties for manipulating the DOM are not part of JS, rather they are part of the WEB APIs
+
+
 
