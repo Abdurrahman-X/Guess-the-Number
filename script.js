2a3dfc76b9be6a4c7d1c8daf773ad2aedb5594e4
Abdurrahman Abolaji <abolajiabdurrahman@gmail.com>
2022-01-11 12:44:22 +0100

    selecting and manipulating elements

--- script.js
+++ script.js
@@ -2,5 +2,16 @@
 
 // The DOM is a complete structured representation of the HTML document. DOM methods and properties for manipulating the DOM are not part of JS, rather they are part of the WEB APIs
 
+// Use the alt + down to move lines of code below another.
+
+document.querySelector('.message').textContent = 'Correct number!🎉';
+document.querySelector('.number').textContent = 24;
+document.querySelector('.score').innerHTML = 40;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+
+
+
 
 
