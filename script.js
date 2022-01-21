e12812b639c028d27e7bab9b3ac9a8359fef5732
Abdurrahman Abolaji <abolajiabdurrahman@gmail.com>
2022-01-21 10:33:34 +0100

    implementing game logic

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ console.log(document.querySelector('.guess').value);
 */
 
     const secretNumber = Math.trunc(Math.random() * 20) + 1;
+    let score = 20;
     document.querySelector('.number').textContent = secretNumber;
     
     console.log(secretNumber);  
@@ -30,11 +31,25 @@ document.querySelector('.check').addEventListener('click', function() {
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = "🎉 Correct Number!"
     } else if (guess > secretNumber) {
-        document.querySelector('.message').textContent = "📈 Too High!"
-        document.querySelector('.score').textContent--;
+        if (score > 1) {
+            document.querySelector('.message').textContent = "📈 Too High!"
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = "💥Game Over!"
+            document.querySelector('.score').textContent = 0;
+        }
+        
     } else if (guess < secretNumber) {
-        document.querySelector('.message').textContent = "📉 Too Low!"
-        document.querySelector('.score').textContent--;
+        if (score > 1) {
+            document.querySelector('.message').textContent = "📉 Too Low!"
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = "💥Game Over!"
+            document.querySelector('.score').textContent = 0;
+        }
+        
     }
 
 })
