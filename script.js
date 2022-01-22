4a932e2d16b551ae203d31305e3b0a5f0c53fcda
Abdurrahman Abolaji <abolajiabdurrahman@gmail.com>
2022-01-22 11:22:31 +0100

    Manipulating CSS styles

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ console.log(document.querySelector('.guess').value);
 
     const secretNumber = Math.trunc(Math.random() * 20) + 1;
     let score = 20;
-    document.querySelector('.number').textContent = secretNumber;
+    
     
     console.log(secretNumber);  
 
@@ -28,8 +28,15 @@ document.querySelector('.check').addEventListener('click', function() {
 //    1. Assume no value was input into the field
     if (!guess) {
         document.querySelector('.message').textContent = "⛔ No Number!"
+
+        // when player guesses correctly
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = "🎉 Correct Number!"
+        document.querySelector('.number').textContent = secretNumber;
+        document.body.style.backgroundColor = "#60b347";
+        document.querySelector(".number").style.width = "25rem"; // this becomes an inline style. the CSS file isnt changed one bit.
+
+        // when guess is higher than secret number
     } else if (guess > secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = "📈 Too High!"
@@ -40,6 +47,7 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.score').textContent = 0;
         }
         
+        // when guess is lower than secret number
     } else if (guess < secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = "📉 Too Low!"
@@ -52,6 +60,15 @@ document.querySelector('.check').addEventListener('click', function() {
         
     }
 
+})
+
+document.querySelector('.again').addEventListener('click', function () {
+    document.querySelector('.message').textContent = "Start guessing..." 
+    document.querySelector('.score').textContent = 20;
+    document.body.style.backgroundColor = "#222";
+    document.querySelector(".number").style.width = "15rem";
+    document.querySelector('.number').textContent = "?";
+    document.querySelector(".guess").value = "";
 })
 // whenever we get something from the user interface e.g value of an input field, the data type is always a string. Hence the need to convert the 'guess' variable into a number
 
